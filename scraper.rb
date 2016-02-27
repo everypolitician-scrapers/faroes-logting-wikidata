8732e081b2712f613a8fc7c29790c6457e837e8d
Tony Bowden <tony@tmtm.com>
2016-02-27 19:14:40 +0000

    Modernise scraper

--- scraper.rb
+++ scraper.rb
@@ -1,10 +1,8 @@
 #!/bin/env ruby
 # encoding: utf-8
 
-require 'pry'
 require 'wikidata/fetcher'
 
 names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/faroes-logting-wikipedia', column: 'identifier__wikipedia_fo')
 EveryPolitician::Wikidata.scrape_wikidata(names: { fo: names }, output: false)
-warn EveryPolitician::Wikidata.notify_rebuilder
 
